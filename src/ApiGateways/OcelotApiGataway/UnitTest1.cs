dd16dffe47650a7d352847f1da05798030b423e8
Bùi Tấn Lân <tanlanhcmus@gmail.com>
2022-11-26 00:37:54 +0700

    feat(): init project

---
+++ src/ApiGateways/OcelotApiGataway/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace OcelotApiGataway;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
